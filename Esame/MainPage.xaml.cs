e0d3085d171f3c8a9c9a05987cd3dfa7cceeacad
HanKumiho <hankumiho.business@gmail.com>
2025-11-14 16:13:14 +0100

    Visualizzazione prodotti su mainpage

--- Esame/MainPage.xaml.cs
+++ Esame/MainPage.xaml.cs
@@ -2,23 +2,43 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        RestService service;
+        private List<Product> _items;
+        public List<Product> Items
+        {
+            get => _items;
+            set
+            {
+                if (_items != value)
+                {
+                    _items = value;
+                    OnPropertyChanged();  // Notifica il cambio della proprietà
+                }
+            }
+        }
 
         public MainPage()
         {
             InitializeComponent();
+            service = new RestService();
+
+            // Impostiamo il BindingContext sulla pagina
+            BindingContext = this;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        // Metodo per caricare i dati quando la pagina viene caricata
+        protected override async void OnAppearing()
         {
-            count++;
+            base.OnAppearing();
+            Items = await service.GetProductsAsync();
+            // Informa il sistema di binding che i dati sono stati aggiornati
+            OnPropertyChanged(nameof(Items));
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private async void OnCounterClicked(object? sender, EventArgs e) 
+        {
+            var service = new RestService();
+            var products = await service.GetProductsAsync();
         }
     }
 
